7a07cfdd41b8300566438275851e0d8f37b2c813
Chilly <reutas39@gmail.com>
2023-04-01 17:42:20 +0200

    v.0.0.4

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import index
+from .views import AdsList, AdsCreate, AdsDetail, AdsUpdate, AdsDelete
 
 urlpatterns = [
-    path('', index),
+    path('', AdsList.as_view(), name='ads_list'),
+    path('<int:pk>', (AdsDetail.as_view()), name='ads'),
+    path('create/', AdsCreate.as_view(), name='ads_create'),
+    path('<int:pk>/edit', AdsUpdate.as_view(), name='ads_edit'),
+    path('<int:pk>/delete', AdsDelete.as_view(), name='ads_delete'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,54 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'indextmp.html')
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from ads.models import Post
+
+menu = [#{'title': "О сайте", 'url_name': 'about'},
+        #{'title': "Добавить статью", 'url_name': 'ads_create'},
+        #{'title': "Обратная связь", 'url_name': 'contact'},
+        #{'title': "Войти", 'url_name': 'login'}
+]
+
+class AdsList(ListView):
+    model = Post
+    ordering = '-created'
+    template_name = 'index.html'
+    context_object_name = 'ads'
+    #paginate_by = 10  # вот так мы можем указать количество записей на странице
+
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     return self.queryset
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['news_count'] = f'Количество статей: {self.filterset.qs.count()}'
+        # # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        # # Добавляем в контекст объект фильтрации.
+        # context['filterset'] = self.filterset
+        # # Добавим ещё одну пустую переменную,
+        # # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        # context['next_sale'] = None
+        # context['title'] = 'Главная страница'
+        # context['cat_selected'] = 0
+        context['menu'] = menu
+        # context['is_author'] = self.request.user.groups.filter(name='authors').exists()
+
+        return context
+
+class AdsDetail(DetailView):
+    pass
+
+class AdsCreate(CreateView):
+    pass
+
+class AdsUpdate(UpdateView):
+    pass
+
+class AdsDelete(DeleteView):
+    pass
